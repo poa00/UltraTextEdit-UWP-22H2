d2e3faa0a10d26d32f5e4eb26fd533569bbe2dab
jpb <jakub.baranski.33@gmail.com>
2022-09-24 07:40:59 +0100

    Update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.0.25120.1000")]
-[assembly: AssemblyFileVersion("10.0.25120.1")]
+[assembly: AssemblyVersion("10.0.25126.1000")]
+[assembly: AssemblyFileVersion("10.0.25126.1")]
 [assembly: ComVisible(false)]
