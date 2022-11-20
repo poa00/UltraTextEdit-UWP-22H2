50a77d91f868f5c13aa78258c2d859f45ff75340
jpb <jakub.baranski.33@gmail.com>
2022-11-20 09:18:17 +0000

    Update What's New panel

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -105,10 +105,10 @@ namespace UltraTextEdit_UWP.Views
 
             WhatsNew.Add(new()
             {
-                Title = "Icons on Superscript and Subscript buttons",
-                Icon = "\uE001",
+                Title = "Comments",
+                Icon = "\uE15F",
                 Tag = "LocAndAcc",
-                Description = $"The superscript and subscript toggle buttons now have icons!"
+                Description = $"Comments functionality is now available! \nJust tap or click the Comments buton to open the pane, then when you're finished, just go into the new Comments ribbon tab and click or tsp the button there to close the pane."
             });
 
             //WhatsNew.Add(new()
