68b2889d3955fec99aa28a317a44701027277f9e
jpb <jakub.baranski.33@gmail.com>
2023-02-01 11:13:53 +0000

    Initial tabs implementation

--- App.xaml.cs
+++ App.xaml.cs
@@ -90,7 +90,7 @@ namespace UltraTextEdit_UWP
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(BasePage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
