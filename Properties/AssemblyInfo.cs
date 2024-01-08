9d3e056f2b77eb3ac1761f6f687e5be5894b23b9
ErrorTek <jakub.baranski.33@gmail.com>
2024-01-08 21:40:21 +0000

    Update app dates to 2024

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jpb")]
 [assembly: AssemblyProduct("UltraTextEdit UWP")]
-[assembly: AssemblyCopyright("Copyright © 2023 jpb")]
+[assembly: AssemblyCopyright("Copyright © 2024 jpb")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
